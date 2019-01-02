1641f9c506fc661e986b163f15aaa52dbec61eff
Razmanugget <rami_s@hotmail.com>
2019-01-02 12:36:18 +0100

    update - experience

--- 13 - resume.playground/Contents.swift
+++ 13 - resume.playground/Contents.swift
@@ -18,22 +18,28 @@ struct Experience {
   }
   
   func printList() {
-    print("hello \(title)")
+    print("\nTitle: \(title)")
+    print("From: \(yearStart) to \(yearEnd)")
+    print("Location: \(location)")
+    for item in 0..<responsibleFor.count {print("Did this: \(responsibleFor[item])")}
   }
 }
 
-var lyfeBug = Experience(title: "Project Lead", location: "Warsaw, Poland", yearStart: 2016, yearEnd: 2018, responsibleFor: ["Swift learning materials for student use", "Website & content for: www.lyfebug.com", "Prototype mockups for mobile apps"])
+var lyfeBug = Experience(title: "Project Lead", location: "Warsaw, Poland", yearStart: 2016, yearEnd: 2018, responsibleFor: ["Swift learning materials for student use", "Website & content for: www.lyfebug.com", "Prototype mockups for mobile apps", "This CV in code"])
 let noteStir = Experience(title: "Founder/Project Manager", location: "Dover, DE", yearStart: 2009, yearEnd: 2015, responsibleFor: ["Front-end UI design for a note sharing wiki web app", "Balancing site needs with Drupal developer team abilities", "Paperwork - financials, Service Mark"])
 let cheminDeSavoir = Experience(title: "Founder and CEO", location: "Las Vegas, NV", yearStart: 1998, yearEnd: 2009, responsibleFor: ["Pricing and trading equities and derivatives", "Tech sector trend analysis", "Corporate financial statment filings"])
-let dell = Experience(title: "Finance Intranet Site Manager", location: "Austin, TX", yearStart: 1995, yearEnd: 1998, responsibleFor: ["Managing a team of 4 in support of a user community of 1200+", "Administered financial databases in Hyperion & Ross", "Leading the migration of Finance to Windows 95 (man, I am sooo old)"])
-
-for (key, value) in techSkills {print("skill: \(key), ability: \(value)")}
-for (key, value) in businessSkills {print("skill: \(key), ability: \(value)")}
-for (key, value) in languageSkills {print("language: \(key), ability: \(value)")}
+let dell = Experience(title: "Finance Intranet Site Manager", location: "Austin, TX", yearStart: 1995, yearEnd: 1998, responsibleFor: ["Managing a team of 4 in support of a user community of 1200+", "Administered financial databases in Hyperion & Ross", "Leading the migration of Finance to Windows 95 (man, I am sooo old)\n"])
 
 lyfeBug.printList()
+noteStir.printList()
+cheminDeSavoir.printList()
+dell.printList()
+
+for (key, value) in techSkills {print("tech skill: \(key), ability: \(value)")}
+for (key, value) in businessSkills {print("business skill: \(key), ability: \(value)")}
+for (key, value) in languageSkills {print("language: \(key), ability: \(value)")}
 
 var hiringDecision = readyToHire ?? true
 if hiringDecision == true {
-  print("Phone: +48 532 364 660\nEmail: rami_s@hotmail.com")
-} else { print("Does not compute...") }
+  print("\nPhone: +48 532 364 660\nEmail: rami_s@hotmail.com")
+} else { print("\n\nDoes not compute.") }
