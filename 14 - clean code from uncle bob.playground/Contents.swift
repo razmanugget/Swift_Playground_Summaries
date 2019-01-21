673bbd9aa5d0fa1a1ee079df107280b90e7ddfe6
Razmanugget <rami_s@hotmail.com>
2019-01-21 21:28:54 +0100

    clean code lesson update - not finished

--- 14 - clean code from uncle bob.playground/Contents.swift
+++ 14 - clean code from uncle bob.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// Listing 2-1 Variables with unclear context
+// Listing 2-1 Variables with unclear context (bad)
 func printGuessStatistics(candidate: Character, count: Int) -> () {
 
     var number: String
@@ -26,3 +26,53 @@ func printGuessStatistics(candidate: Character, count: Int) -> () {
 }
 
 printGuessStatistics(candidate: "h", count: 1)
+
+// Listing 2-2 Variables have a context (good) -> changed program to spell out numbers
+class GuessStatisticsMessage {
+    var number: String
+    var verb: String
+    var pluralModifier: String
+
+    init(number: String,
+         verb: String,
+         pluralModifier: String) {
+        self.number = number
+        self.verb = verb
+        self.pluralModifier = pluralModifier
+    }
+
+    func make(candidate: Character, letters: Int) -> String {
+        createPluralDependentMessageParts(letters: letters)
+        return ("There \(verb) \(number) \(pluralModifier)letter \(candidate).")
+    }
+
+    func createPluralDependentMessageParts(letters: Int) -> () {
+        if (letters == 0) {
+            thereAreNoLetters()
+        } else if (letters == 1) {
+            thereIsOneLetter()
+        } else {
+            thereAreManyLetters(letters: letters)
+        }
+    }
+
+    func thereAreManyLetters(letters: Int) -> () {
+        number = (letters).count
+        verb = "are"
+        pluralModifier = "s"
+    }
+
+    func thereIsOneLetter() -> () {
+        number = "1"
+        verb = "is"
+        pluralModifier = ""
+    }
+
+    func thereAreNoLetters() -> () {
+        number = "no"
+        verb = "are"
+        pluralModifier = "s"
+    }
+}
+
+
