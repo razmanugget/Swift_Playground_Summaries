3db1bb7498e0fd9b8867a3211dd6ef769218eb50
Razmanugget <rami_s@hotmail.com>
2019-01-22 11:08:21 +0100

    simplified example 2-1

--- 14 - clean code from uncle bob.playground/Contents.swift
+++ 14 - clean code from uncle bob.playground/Contents.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 // Listing 2-1 Variables with unclear context (bad)
-func printGuessStatistics(candidate: Character, count: Int) -> () {
+func printGuessStatistics(word: String) -> () {
 
     var number: String
     var verb: String
     var pluralModifier: String
 
-    if (count == 0) {
+    if (word.count == 0) {
         number = "no"
         verb = "are"
         pluralModifier = "s"
-    } else if (count == 1) {
+    } else if (word.count == 1) {
         number = "1"
         verb = "is"
         pluralModifier = ""
@@ -21,58 +21,58 @@ func printGuessStatistics(candidate: Character, count: Int) -> () {
         pluralModifier = "s"
     }
 
-    let guessMessage = "There \(verb) \(number) \(pluralModifier)letter \(candidate)."
+    let guessMessage = "There \(verb) \(number) letter\(pluralModifier) in \(word)."
     print(guessMessage)
 }
+printGuessStatistics(word: "hello")
 
-printGuessStatistics(candidate: "h", count: 1)
 
 // Listing 2-2 Variables have a context (good) -> changed program to spell out numbers
-class GuessStatisticsMessage {
-    var number: String
-    var verb: String
-    var pluralModifier: String
-
-    init(number: String,
-         verb: String,
-         pluralModifier: String) {
-        self.number = number
-        self.verb = verb
-        self.pluralModifier = pluralModifier
-    }
-
-    func make(candidate: Character, letters: Int) -> String {
-        createPluralDependentMessageParts(letters: letters)
-        return ("There \(verb) \(number) \(pluralModifier)letter \(candidate).")
-    }
-
-    func createPluralDependentMessageParts(letters: Int) -> () {
-        if (letters == 0) {
-            thereAreNoLetters()
-        } else if (letters == 1) {
-            thereIsOneLetter()
-        } else {
-            thereAreManyLetters(letters: letters)
-        }
-    }
-
-    func thereAreManyLetters(letters: Int) -> () {
-        number = (letters).count
-        verb = "are"
-        pluralModifier = "s"
-    }
-
-    func thereIsOneLetter() -> () {
-        number = "1"
-        verb = "is"
-        pluralModifier = ""
-    }
-
-    func thereAreNoLetters() -> () {
-        number = "no"
-        verb = "are"
-        pluralModifier = "s"
-    }
-}
+//class GuessStatisticsMessage {
+//    var number: String
+//    var verb: String
+//    var pluralModifier: String
+//
+//    init(number: String,
+//         verb: String,
+//         pluralModifier: String) {
+//        self.number = number
+//        self.verb = verb
+//        self.pluralModifier = pluralModifier
+//    }
+//
+//    func make(candidate: Character, letters: Int) -> String {
+//        createPluralDependentMessageParts(letters: letters)
+//        return ("There \(verb) \(number) letter\(pluralModifier).")
+//    }
+//
+//    func createPluralDependentMessageParts(letters: Int) -> () {
+//        if (letters == 0) {
+//            thereAreNoLetters()
+//        } else if (letters == 1) {
+//            thereIsOneLetter()
+//        } else {
+//            thereAreManyLetters(letters: letters)
+//        }
+//    }
+//
+//    func thereAreManyLetters(letters: Int) -> () {
+//        number = (letters).count
+//        verb = "are"
+//        pluralModifier = "s"
+//    }
+//
+//    func thereIsOneLetter() -> () {
+//        number = "1"
+//        verb = "is"
+//        pluralModifier = ""
+//    }
+//
+//    func thereAreNoLetters() -> () {
+//        number = "no"
+//        verb = "are"
+//        pluralModifier = "s"
+//    }
+//}
 
 
