59d72e2cc64fb4227e1218fd173628715c5e3cea
Razmanugget <rami_s@hotmail.com>
2019-01-28 19:19:22 +0100

    finished day 4 code challenge

--- 15 - hackerrank stuff.playground/Contents.swift
+++ 15 - hackerrank stuff.playground/Contents.swift
@@ -177,3 +177,43 @@ if N % 2 == 0 {
 } else {
     print("Weird")
 }
+
+
+// Day 4 - HackerRank - class vs instance
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        if initialAge <= 0 {
+            age = 0
+            print("Age is not valid, setting age to 0..")
+        } else {
+            age = initialAge
+        }
+    }
+
+    func amIOld() {
+        if age < 13 {
+            print("You are young..")
+        } else if age >= 13 && age < 18 {
+            print("You are a teenager")
+        } else {
+            print("You are old..")
+        }
+    }
+
+    func yearPasses() {
+        age += 1
+    }
+}
+
+let age = 41
+let p = Person(initialAge: age)
+
+p.amIOld()
+
+for _ in 1...3 {
+    p.yearPasses()
+}
+
+p.amIOld()
