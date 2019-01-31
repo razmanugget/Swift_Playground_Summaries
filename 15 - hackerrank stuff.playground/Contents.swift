d902e031567b571fc6bfea7b3bb2f9e3f1ff10f3
Razmanugget <rami_s@hotmail.com>
2019-01-31 21:07:43 +0100

    start day 6

--- 15 - hackerrank stuff.playground/Contents.swift
+++ 15 - hackerrank stuff.playground/Contents.swift
@@ -222,3 +222,28 @@ for i in 1...10 {
     print("\(newLoop) x \(i) = \(i * newLoop)")
 }
 
+
+// Day 6 - HackerRank - review
+let numStrings = "Hacker Rank"
+
+func printEvenAndOdd(string: String) {
+    // This prints inputString to stderr for debugging:
+    fputs("string: " + string + "\n", stderr)
+
+    // Print the even-indexed characters
+    // Write your code here
+
+    // Print a space
+    print(" ", terminator: "")
+
+    // Print the odd-indexed characters
+    // Write your code here
+
+    // Print a newline
+    print()
+}
+
+for _ in 1...numStrings {
+    let inputString = readLine()!
+    printEvenAndOdd(string: inputString)
+}
