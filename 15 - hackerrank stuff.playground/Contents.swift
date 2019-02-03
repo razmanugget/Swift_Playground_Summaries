ffb3295a640400baad4791a372ca25a68e9cb6df
Razmanugget <rami_s@hotmail.com>
2019-02-03 21:52:02 +0100

    update tweaks

--- 15 - hackerrank stuff.playground/Contents.swift
+++ 15 - hackerrank stuff.playground/Contents.swift
@@ -226,41 +226,41 @@ for i in 1...10 {
 // Day 6 - HackerRank - review
 let numStrings = "Hacker Rank"
 
-func printEvenAndOdd(string: String) {
-    // This prints inputString to stderr for debugging:
-    fputs("string: " + string + "\n", stderr)
-
-    // Print the even-indexed characters
-    let x
-
-    // Write your code here
-
-    // Print a space
-    print(" ", terminator: "")
-
-    // Print the odd-indexed characters
-
-    // Write your code here
-
-    // Print a newline
-    print()
-}
-
-for _ in 1...numStrings {
-    let inputString = readLine()!
-    printEvenAndOdd(string: inputString)
-}
+//func printEvenAndOdd(string: String) {
+//    // This prints inputString to stderr for debugging:
+//    fputs("string: " + string + "\n", stderr)
+//
+//    // Print the even-indexed characters
+//    let x
+//
+//    // Write your code here
+//
+//    // Print a space
+//    print(" ", terminator: "")
+//
+//    // Print the odd-indexed characters
+//
+//    // Write your code here
+//
+//    // Print a newline
+//    print()
+//}
+//
+//for _ in 1...numStrings {
+//    let inputString = readLine()!
+//    printEvenAndOdd(string: inputString)
+//}
 
 
 // Day 7 - HackerRank - arrays
-//guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
-//    else { fatalError("Bad input") }
-//
-//guard let arrTemp = readLine() else { fatalError("Bad input") }
-//let arr: [Int] = arrTemp.split(separator: " ").map {
-//    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
-//        return arrItem
-//    } else { fatalError("Bad input") }
-//}
-//
-//guard arr.count == n else { fatalError("Bad input") }
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+guard let arrTemp = readLine() else { fatalError("Bad input") }
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
